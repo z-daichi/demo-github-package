3932dad9b142badbfca9ed79076900008fdbe77a
岩元大地 <daichi-iwamoto@ZEN-24-PC200.local>
2025-05-29 18:48:40 +0900

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-red-200'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
